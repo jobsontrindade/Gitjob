c1a60d6e1e47a33688241cde1029177d82d9d4fd
ALUNO <ALUNO@C-LAB05-PC06>
2017-03-31 21:26:44 -0300

    Jobson

---
+++ src/praticagit2/Outro.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praticagit2;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class Outro {
+    public static void main(String[] args) {
+        System.out.println("testeGIt");
+    }
+}
